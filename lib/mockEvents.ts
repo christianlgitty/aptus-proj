150ede748cca7004065fe03b611aba11ca0344f7
Christian Lange <christianlangebiz@gmail.com>
2025-12-21 19:15:12 -0600

    feat: introduce canonical event data model

---
+++ lib/mockEvents.ts
@@ -0,0 +1,32 @@
+import { Event } from "./types";
+
+export const mockEvents: Event[] = [
+  {
+    id: "boston-marathon",
+    name: "Boston Marathon",
+    location: "Boston, MA",
+    date: "Apr 20, 2026",
+    sport: "Running",
+  },
+  {
+    id: "nyc-marathon",
+    name: "New York City Marathon",
+    location: "New York, NY",
+    date: "Nov 1, 2026",
+    sport: "Running",
+  },
+  {
+    id: "ironman-kona",
+    name: "Ironman World Championship",
+    location: "Kailua-Kona, HI",
+    date: "Oct 10, 2026",
+    sport: "Triathlon",
+  },
+  {
+    id: "unbound-gravel",
+    name: "Unbound Gravel",
+    location: "Emporia, KS",
+    date: "Jun 6, 2026",
+    sport: "Cycling",
+  },
+];
